87a5464b65366102553aa87d09e70c0b0c924c64
DevPedroSouza <dev.pedrosouzza3@gmail.com>
2025-11-21 21:20:14 -0300

    Adicionar novas pastas e arquivos (add task)

---
+++ todolist/src/main/java/br/com/devpedrosouza/todolist/task/TaskController.java
@@ -0,0 +1,23 @@
+package br.com.devpedrosouza.todolist.task;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public TaskModel create(@RequestBody TaskModel taskModel) {
+        var task = this.taskRepository  .save(taskModel);
+        return task;
+    }
+
+}
---
+++ todolist/src/main/java/br/com/devpedrosouza/todolist/task/TaskModel.java
@@ -0,0 +1,32 @@
+package br.com.devpedrosouza.todolist.task;
+
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import lombok.Data;
+import jakarta.persistence.Column;
+
+@Data
+@Entity(name = "tb_task")
+public class TaskModel {
+    
+    @Id
+    @GeneratedValue(generator = "UUID")
+
+    private UUID id;
+    private String description;
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
