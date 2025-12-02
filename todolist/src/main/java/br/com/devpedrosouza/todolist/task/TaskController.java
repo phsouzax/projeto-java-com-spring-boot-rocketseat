f43f00a7e7a74b4b443da5be0e0a66a8c06e6231
DevPedroSouza <dev.pedrosouzza3@gmail.com>
2025-12-01 23:30:04 -0300

    feat: hours of validation

--- todolist/src/main/java/br/com/devpedrosouza/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/devpedrosouza/todolist/task/TaskController.java
@@ -5,8 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 import jakarta.servlet.http.HttpServletRequest;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
+
+import org.springframework.cglib.core.Local;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 
 @RestController
 @RequestMapping("/tasks")
@@ -16,12 +24,20 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+
+        var currentDate = LocalDateTime.now();
+
+        if (currentDate.isAfter(taskModel.getStartAt()) || (currentDate.isAfter(taskModel.getEndAt()))) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+              .body("A data de início / data de término não pode ser anterior a data atual");
+
+        }
+
 
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
-
 }
